0be064c7039cb00a4c3d096612399116a6289cb6
NAWENANDARAUNG-\NaweNandarAung <scm.nwaenandaraung@gmail.com>
2021-05-27 16:53:59 +0630

    13-Most-Recent-or-Most-Popular

--- app/Queries/CommunityLinksQuery.php
+++ app/Queries/CommunityLinksQuery.php
@@ -8,17 +8,13 @@ class CommunityLinksQuery
 {
     public function get($sortByPopular, $channel) 
     {
-        $orderBy = $sortByPopular ? 'vote_count': 'updated_at';
+        $orderBy = $sortByPopular ? 'votes_count': 'updated_at';
 
         return CommunityLink::with('votes','creator','channel')
+                            ->withCount('votes')
                             ->forChannel($channel)
                             ->where('approved',1)
-                            ->leftJoin('community_links_votes','community_links_votes.community_link_id', '=','community_links.id')
-                            ->selectRaw(
-                                'community_links.*, count(community_links_votes.id) as vote_count'
-                            )
-                            ->orderBy('vote_count', 'desc')
-                            ->groupBy('community_links.id')
+                            ->orderBy($orderBy, 'desc')
                             ->paginate(3);
     }
 }
--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -18,6 +18,14 @@
                                 <span class="text-xl font-bold">&#8211; {{ $channel->title }}</span>
                             @endif
 
+                            <ul class= "flex">
+                                <li class="mr-6 {{request()->exists('popular')? 'bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold':'tbg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold' }}">
+                                    <a href="{{request()->url()}}">Most Recent</a>
+                                </li>
+                                <li class="mr-6 {{request()->exists('popular')? 'tbg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold':'bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold'}}">
+                                    <a href="?popular&page={{ $links->currentPage() }}">Most Popular</a>
+                                </li>
+                            </ul>
                             @include('community.list')
                         </div>
                         @include('community.add-link')
